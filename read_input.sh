e3005364fabaff170e84a20e8e834d4f9c9c2ad1
KiranKumar <balakirankumar999@gmail.com>
2020-09-03 20:38:05 +0530

    Reading Input

---
+++ read_input.sh
@@ -0,0 +1,8 @@
+echo "What is your name ?"
+read name
+echo "What is your password ?"
+read -s pass
+read -p "What is your favorite animal " animal
+echo "Name     : $name"
+echo "Password : $pass"
+echo "Animal   : $animal"
